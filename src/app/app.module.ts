98eee114ad2c2c46be1f0337a5f2599be81f1b62
ThijsvanDam <thijsvandam@msn.com>
2020-05-20 10:12:08 +0200

    Add e2e and lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DynamicMapComponent } from './dynamic-map/dynamic-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicMapComponent
   ],
   imports: [
     BrowserModule,
