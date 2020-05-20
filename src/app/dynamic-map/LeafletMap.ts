fc30875723aeb4cf2d7b21a1c9178f822cfa93ae
ThijsvanDam <thijsvandam@msn.com>
2020-05-20 11:07:33 +0200

    Create LeafletMap

--- src/app/dynamic-map/LeafletMap.ts
+++ src/app/dynamic-map/LeafletMap.ts
@@ -1,3 +1,23 @@
+import * as L from 'leaflet';
+
 export class LeafletMap {
-  
+  private map;
+
+  constructor(center: [number, number], zoom: number) {
+    this.map = L.map('map', {
+      center: center,
+      zoom: zoom
+    });
+
+   }
+
+   public addTileLayer(url, attr){
+     
+    const tiles = L.tileLayer(url, {
+      maxZoom: 19,
+      attribution: attr
+    });
+
+    tiles.addTo(this.map);
+   }
 }
--- src/app/dynamic-map/dynamic-map.component.ts
+++ src/app/dynamic-map/dynamic-map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as Map from './LeafletMap';
+import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
@@ -10,22 +11,12 @@ export class DynamicMapComponent implements AfterViewInit {
   private map;
 
   constructor() { }
+
   ngAfterViewInit(): void {
-    this.initMap();
+    this.map = new Map.LeafletMap([39.8282, -98.5795], 3);
+    this.map.addTileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      );
   }
-
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
-  }
-
 }
